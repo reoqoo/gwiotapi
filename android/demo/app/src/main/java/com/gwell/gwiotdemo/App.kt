4a6e0354aac9e60de8fece0017588a55c4aa700a
XIAOLEI <xiaolei@gwell.cc>
2025-06-15 17:18:00 +0800

    feature： 升级GWIot的版本，以及YooseeKit的版本

--- android/demo/app/src/main/java/com/gwell/gwiotdemo/App.kt
+++ android/demo/app/src/main/java/com/gwell/gwiotdemo/App.kt
@@ -24,11 +24,26 @@ class App : Application() {
                     position = WaterMarkPosition.RIGHT_TOP
                 )
             )
-            this["iotAssessId"] = ""
+            // iotAssessId和iotAccessToken决定了监控鉴权是否发起成功
+            this["iotAssessId"] = "-"
             this["iotAccessToken"] = ""
-            this["region"] = ""
-            this["regRegion"] = ""
+            // region 和 regRegion 用来与后台通信时认证使用
+            this["region"] = "CN"
+            this["regRegion"] = "CN"
+            // 这个KEY决定了监控内部某些包含APP名称的字符串
             this["KEY_HOST_APPNAME_RES"] = R.string.app_name
+            // 这个很重要，跟后台通信会携带过去
+            this["KEY_APPLICATION_ID"] = ""
+            // 这个是决定了后台服务接口的地址
+            this["KEY_BASE_URL"] = ""
+            // 向平台注册的APPID
+            this["APP_ID"] = ""
+            // 向平台注册的APP_TOKEN
+            this["APP_TOKEN"] = ""
+            // 向平台注册的APP_NAME
+            this["KEY_APP_NAME"] = ""
+            // 当前国家/地区的缩写，比如中国(cn)
+            this["KEY_COUNTRY_SHORT_NAME"] = "cn"
         })
 
         // just for test watermask
