b4ef80afbceb2e530ef309309c3249779cbb555e
yangyifei <yangyifei@yoosee.com>
2026-01-08 16:56:01 +0800

    fix：（iptime）使用新接口设置主题颜色

--- ios/demo/GWIoTAPIDemo/Classes/AppEntrance/AppInfo.swift
+++ ios/demo/GWIoTAPIDemo/Classes/AppEntrance/AppInfo.swift
@@ -17,9 +17,12 @@ extension UIApplication {
             app: nil,
             accountSharedIcon: nil
         )
+        
+        let colors = GWIoTApi.Theme.Colors(brand: "#279d44", brandHighlight: "#238b3c", brandDisable: "#dfe1e7", brand2: "#279d44", brand2Highlight: "#238b3c", brand2Disable: "#dfe1e7", text: nil, secondaryText: nil, tertiaryText: nil, lightText: nil, linkText: nil, maskBackground: nil, hudBackground: nil, inputLineDisable: nil, inputLineEnable: nil, separatorLine: nil, mainBackground: nil, secondaryBackground: nil, stateSafe: nil, stateWarning: nil, stateError: nil)
+
 
         return .init(
-            theme: Theme(colors: nil, icons: nil),
+            theme: Theme(colors: colors, icons: nil),
             texts: AppTexts(appNamePlaceHolder: "Defender ClearVu")
         )
 
